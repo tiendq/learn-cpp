89bff7c7e2c97afb5d81268a5e98e8c68e1e489b
Tien Do <tiendq@gmail.com>
2024-03-12 20:18:50 +0700

    add CMake configuration

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+// Add cmake CLI to terminal: sudo "/Applications/CMake.app/Contents/bin/cmake-gui" --install=/usr/local/bin
+
+int main(int, char**) {
+  std::cout << "Hello, from learn-cpp!\n";
+}
